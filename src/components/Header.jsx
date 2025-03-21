3e495b7cfdc7e25d3db27c815ec959ffc997d353
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-03-21 17:14:01 +0100

    Add: Css Custom [Header e Body]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,22 @@ import logo from "../assets/imgs/boolroad.svg";
 
 export default function Header() {
   return (
-    <header>
-      <nav className="navbar navbar-expand">
-        <div className="nav navbar-nav d-flex align-items-center">
+    <header className="p-3 shadow-sm">
+      <nav className="navbar navbar-expand nav-custom">
+        <div className=" navbar-nav d-flex align-items-center ">
           <NavLink className="navbar-brand" to="/">
-            <img
-              className="logo"
-              src={logo}
-              alt="brand"
-              width={75}
-            />
+            <img className="logo" src={logo} alt="brand" width={150} />
           </NavLink>
-          <NavLink className="nav-item nav-link nav-route" to="/">
+          <NavLink className="home" to="/">
             Home
           </NavLink>
+        </div>
+        <div className="navbar-nav">
+          <input
+            type="text"
+            class="form-control w-100  search-custom"
+            placeholder="Cerca..."
+          ></input>
         </div>
       </nav>
     </header>
