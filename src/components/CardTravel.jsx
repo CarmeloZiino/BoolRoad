70a888ac9b476601e4f03da7ab90ed8e3fe35f8d
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-03-24 15:49:08 +0100

    add: css complete

--- src/components/CardTravel.jsx
+++ src/components/CardTravel.jsx
@@ -3,40 +3,31 @@ import { Link } from "react-router-dom";
 
 export default function CardTravel() {
   return (
-    <>
-      <div className="container-fluid d-flex flex-column justify-content-center align-items-center" >
-        <h2 className="my-4 title-custom text-center">I Nostri Viaggi</h2>
-        <div className="row gap-3">
-          {travels.map((viaggio) => {
-            return (
-              <>
-                <div className="card pb-4 gap-5 col-4" key={viaggio.id}>
-                  <div className="card-body">
-                    <p className="title-card text-center">{viaggio.citta}</p>
-
-                    <div className="text-center">
-                      <p className="text-card">
-                        {" "}
-                        data di partenza:{" "}
-                        <p className="title-dett">{viaggio.dataPartenza}</p>
-                      </p>
-                      <p className="text-card">
-                        data di arrivo:{" "}
-                        <p className="title-dett">{viaggio.dataArrivo}</p>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="text-center">
-                    <Link className="button mb-3" to={`/travels/${viaggio.id}`}>
-                      vedi i partecipanti
-                    </Link>
-                  </div>
-                </div>
-              </>
-            );
-          })}
-        </div>
+    <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
+      <h2 className="my-4 title-custom text-center">I Nostri Viaggi</h2>
+      <div className="row justify-content-center">
+        {travels.map((viaggio) => (
+          <div key={viaggio.id} className="card pb-4 col-md-3 mx-3">
+            <div className="card-body">
+              <p className="title-card text-center">{viaggio.citta}</p>
+              <div className="text-center">
+                <p className="text-card">
+                  Data di partenza: <span className="title-dett">{viaggio.dataPartenza}</span>
+                </p>
+                <p className="text-card">
+                  Data di arrivo: <span className="title-dett">{viaggio.dataArrivo}</span>
+                </p>
+              </div>
+            </div>
+            <div className="text-center">
+              <Link className="button mb-3" to={`/travels/${viaggio.id}`}>
+                Vedi i partecipanti
+              </Link>
+            </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
+
