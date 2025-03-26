a6c1bd811e6f4440720764d7a0dfd5432c0fe8f9
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-03-26 14:27:24 +0100

    Add: FInal

--- src/components/CardTravel.jsx
+++ src/components/CardTravel.jsx
@@ -7,7 +7,7 @@ export default function CardTravel() {
       <h2 className="my-4 title-custom text-center display-2">I Nostri Viaggi</h2>
       <div className="row justify-content-center">
         {travels.map((viaggio) => (
-          <div key={viaggio.id} className="card pb-4 col-md-3 mx-3">
+          <div key={viaggio.id} className="card pb-4 col-sm-7 col-md-3 mx-3">
             <div className="card-body d-flex flex-column align-items-center">
               <img id="imgTravel" src={viaggio.image} alt="" />
               <p className="title-card text-center h4">{viaggio.citta}</p>
